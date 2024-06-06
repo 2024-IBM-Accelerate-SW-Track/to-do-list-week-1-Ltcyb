a6dcbe56a3130898d1704b20cfa57dc7487bc6f9
arnold <arnoldcai@berkeley.edu>
2024-06-06 02:54:21 -0700

    created about page + pfp

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,26 @@
-import React, { Component } from 'react';
-  
+import React, { Component } from "react";
+import "./About.css";
+import pfp from "../assets/pfp.jpg";
 export default class About extends Component {
-  render() {
-    return (
-      <div>
-        <p>Design your About me page </p> 
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div>
+				<div className="split left">
+					<div className="centered">
+						<img
+							className="profile_image"
+							src={pfp}
+							alt="Profile Pic"
+						/>
+					</div>
+				</div>
+				<div className="split right">
+					<div className="centered">
+						<div className="name_title">Arnold Cai</div>
+						<div className="brief_description">Hi, I'm Arnold.</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
